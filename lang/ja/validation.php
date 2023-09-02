aa80f1b07be92fbd80fd4004f4634be8b0101fd9
kanChome <study.hellbird@gmail.com>
2023-09-02 21:41:50 +0900

    「基礎から学ぶLaravel」p147

---
+++ lang/ja/validation.php
@@ -0,0 +1,23 @@
+<?php
+return [
+    'exists' => '正しい:attributeを選択してください。',
+    'max' => [
+        'numeric' => ':attributeは:max以下で入力してください。',
+        'string' => ':attributeは:max文字以下で入力してください。',
+    ],
+    'min' => [
+        'numeric' => ':attributeは:min以上で入力してください。',
+        'string' => ':attributeは:min文字以上で入力してください。',
+    ],
+    'numeric' => ':attributeは数値で入力してください。',
+    'required' => ':attributeは必須入力です。',
+    'unique' => 'その:attributeは既に登録されています。',
+
+    'attributes' => [
+        'author_id' => '著者ID',
+        'body' => '本文',
+        'category_id' => 'カテゴリーID',
+        'price' => '価格',
+        'title' => 'タイトル',
+    ],
+];
