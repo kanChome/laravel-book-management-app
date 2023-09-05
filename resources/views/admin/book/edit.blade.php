47f5a74ebe87289179edcc96658df304a09c4892
kanChome <study.hellbird@gmail.com>
2023-09-05 21:59:34 +0900

    「基礎から学ぶLaravel」p206

--- resources/views/admin/book/edit.blade.php
+++ resources/views/admin/book/edit.blade.php
@@ -12,40 +12,7 @@
     <form action="{{ route('book.update', $book) }}" method="POST">
         @csrf
         @method('PUT')
-        <div>
-            <label>カテゴリ</label>
-            <select name="category_id">
-                @foreach($categories as $category)
-                    <option value="{{ $category->id }}"
-                    @selected($category->id == old('category_id', $book->category_id))>
-                        {{ $category->title }}
-                    </option>
-                @endforeach
-            </select>
-        </div>
-        <div>
-            <label>タイトル</label>
-            <input type="text" name="title" value="{{ old('title', $book->title) }}">
-        </div>
-        <div>
-            <label>価格</label>
-            <input type="text" name="price" value="{{ old('price', $book->price) }}">
-        </div>
-        <div>
-            <label>著者</label>
-            <ul>
-                @foreach($authors as $author)
-                    <li>
-                        <label>
-                            <input type="checkbox" name="authors[]"
-                                   value="{{ $author->id }}"
-                                @checked(in_array($author->id, old('authors', $authorIds)))>
-                        </label>
-                        {{ $author->name }}
-                    </li>
-                @endforeach
-            </ul>
-        </div>
+        <x-book-form :$categories :$book :$authors :$authorIds />
         <input type="submit" value="更新">
     </form>
 </x-layouts.book-manager>
